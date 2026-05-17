a61f2f4b5cbb5cd4be2cc1240fbff471fd878144
Ahadx488 <bytewise58@gmail.com>
2026-05-17 12:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (21.15%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    bool dfs(vector<int>& arr, int idx, vector<int>& vis) {
+
+        int n = arr.size();
+
+        if(idx < 0 || idx >= n || vis[idx])
+            return false;
+
+        if(arr[idx] == 0)
+            return true;
+
+        vis[idx] = 1;
+
+        return dfs(arr, idx + arr[idx], vis) ||
+               dfs(arr, idx - arr[idx], vis);
+    }
+
+    bool canReach(vector<int>& arr, int start) {
+
+        vector<int> vis(arr.size(), 0);
+
+        return dfs(arr, start, vis);
+    }
+};
