cd053ab99592ff1f8afab02c2f91ebff99ba816c
Ahadx488 <bytewise58@gmail.com>
2026-07-05 09:49:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (60.84%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,20 +2,33 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prodArr(n);
-        vector<int> prefix(n,1),suffix(n,1);
+        vector<int> prodArr(n,1);
+        // vector<int> prefix(n,1),suffix(n,1);
+
+        // for(int i = 1; i < n; i++){
+        //     prefix[i] = prefix[i-1] * nums[i-1];
+        // }
+
+        // for(int i = n-2; i >= 0 ; i--){
+        //     suffix[i] = suffix[i+1] * nums[i+1];
+        // }
+
+        // for(int i = 0; i < n; i++){
+        //     prodArr[i] = prefix[i] * suffix[i];
+        //}
 
         for(int i = 1; i < n; i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+            prodArr[i] = prodArr[i - 1] * nums[i - 1];
         }
 
-        for(int i = n-2; i >= 0 ; i--){
-            suffix[i] = suffix[i+1] * nums[i+1];
+        int suffix = 1;
+
+        for(int i = n - 2; i >= 0; i--){
+            suffix = suffix * nums[i+1];
+            prodArr[i] = prodArr[i] * suffix;
         }
 
-        for(int i = 0; i < n; i++){
-            prodArr[i] = prefix[i] * suffix[i];
-        }
+
         return prodArr;
     }
 };
