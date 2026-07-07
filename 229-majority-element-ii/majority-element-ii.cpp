9b51d3032e76e68c4eda338247fabb04ee3c4c43
Ahadx488 <bytewise58@gmail.com>
2026-07-07 12:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (83.81%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -21,15 +21,50 @@ public:
         // }
         // return ans;
 
-        unordered_map<int,int> mpp;
+        // unordered_map<int,int> mpp;
 
-        for(int x: nums){
-            mpp[x]++;
+        // for(int x: nums){
+        //     mpp[x]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second > n / 3) ans.push_back(it.first);
+        //     if(ans.size() == 2) break;
+        // }
+
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 , ele2;
+
+        for(int i = 0; i < n; i++){
+            if(cnt1 == 0 && ele2 != nums[i]){
+                ele1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0 && ele1 != nums[i]){
+                ele2 = nums[i];
+                cnt2 = 1;
+            }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
-        for(auto it : mpp){
-            if(it.second > n / 3) ans.push_back(it.first);
-            if(ans.size() == 2) break;
+        int count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == ele1 ){
+                count1++;
+            }
+            if(nums[i] == ele2) count2++;
         }
+        if(count1 > floor(n / 3)){
+            ans.push_back(ele1);
+        }
+        if(count2 > floor(n / 3) ){
+            ans.push_back(ele2);
+        }
+        
+
         return ans;
     }
 };
