5df50caf04f862de3c967bf59cc5e7f4bde0f1c8
Ahadx488 <bytewise58@gmail.com>
2026-07-07 11:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (90.11%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,19 +1,38 @@
 class Solution {
+private:
+    vector<int> generateRow(int row){
+        vector<int> temp;
+        long long ans = 1;
+        temp.push_back(1);
+        for(int col = 1; col < row; col++){
+            ans = ans * (row - col);
+            ans = ans/col;
+            temp.push_back(ans);
+        }
+        return temp;
+    }
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> pascalTriangle;
-        int i = 0;
-        while(i < numRows){
-            vector<int> row(i + 1,1);
+        // int i = 0;
+        // while(i < numRows){
+        //     vector<int> row(i + 1,1);
 
-            for(int j = 1; j < i; j++){
-                row[j] = pascalTriangle[i - 1][j-1] + pascalTriangle[i - 1][j];
-            }
+        //     for(int j = 1; j < i; j++){
+        //         row[j] = pascalTriangle[i - 1][j-1] + pascalTriangle[i - 1][j];
+        //     }
 
-            pascalTriangle.push_back(row);
-            i++;
+        //     pascalTriangle.push_back(row);
+        //     i++;
+        // }
+
+        // return pascalTriangle;
+        //pascalTriangle.push_back({1});
+        
+        
+        for(int i = 1; i <= numRows; i++){
+            pascalTriangle.push_back(generateRow(i));
         }
-
         return pascalTriangle;
     }
 };
