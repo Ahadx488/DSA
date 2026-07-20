0d5f60ea0d331dbedfb14f7367ee506a99c02465
Ahadx488 <bytewise58@gmail.com>
2026-07-20 18:06:05 +0530

    Time: 11 ms (14.97%) | Memory: 14.9 MB (96.40%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -15,27 +15,78 @@ public:
 */
 
 class Solution {
+private:
+    Node* insertCopiesInBetween(Node* head){
+        Node* temp = head;
+        while(temp != nullptr){
+            Node* copyNode = new Node(temp -> val);
+            Node* nextNode = temp ->next;
+            temp -> next = copyNode;
+            copyNode -> next = nextNode;
+            temp = nextNode;
+        }
+
+        return head;
+    }
+    Node* connectRandomPointers(Node* head){
+        Node* temp = head;
+
+        while(temp != nullptr){
+            temp->next->random = (temp->random) ? temp->random->next : nullptr;
+            temp = temp -> next -> next;
+        }
+        return head;
+    }
+
+    Node* connectNextPointers(Node* head){
+        Node* temp = head;
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+
+        while(temp != nullptr){
+            res -> next = temp -> next;
+            temp -> next = temp -> next -> next;
+            res = res -> next;
+            temp = temp -> next;
+
+        }
+
+        return dummyNode -> next;
+    }
 public:
     Node* copyRandomList(Node* head) {
-        Node* temp =head;
-        unordered_map<Node*, Node*> mpp;
+        // Node* temp =head;
+        // unordered_map<Node*, Node*> mpp;
 
-        while(temp != nullptr){
-            Node* newNode = new Node(temp -> val);
-            mpp[temp] = newNode;
-            temp = temp -> next;
-        }
+        // while(temp != nullptr){
+        //     Node* newNode = new Node(temp -> val);
+        //     mpp[temp] = newNode;
+        //     temp = temp -> next;
+        // }
 
-        temp = head;
+        // temp = head;
 
-        while(temp != nullptr){
-            Node* copyNode = mpp[temp];
-            copyNode -> next = mpp[temp -> next];
-            copyNode -> random = mpp[temp -> random];
+        // while(temp != nullptr){
+        //     Node* copyNode = mpp[temp];
+        //     copyNode -> next = mpp[temp -> next];
+        //     copyNode -> random = mpp[temp -> random];
 
-            temp = temp -> next;
+        //     temp = temp -> next;
 
-        }
-        return mpp[head];
+        // }
+        // return mpp[head];
+
+        // 1. insert copies of node in between
+
+        Node* temp = insertCopiesInBetween(head);
+         
+        // 2. connect random pointers
+
+        temp = connectRandomPointers(temp);
+
+        // 3. connect next Pointers
+
+        temp = connectNextPointers(temp);
+        return temp;
     }
 };
