b7656bf9126738c0cc4bab67dbd9e0c76e906c66
Ahadx488 <bytewise58@gmail.com>
2026-07-30 09:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (61.00%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,62 @@
+class Solution {
+private:
+    void dfs(int row,  int col, vector<vector<char>> &board, vector<vector<int>> &vis){
+        vis[row][col] = 1;
+        int m = board.size();
+        int n = board[0].size();
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+
+        for(int i = 0; i < 4; i++){
+            int nr = row + dr[i];
+            int nc = col + dc[i];
+
+            if(nr >= 0 && nr < m && nc >= 0 && nc < n && !vis[nr][nc] && board[nr][nc] == 'O'){
+                dfs(nr, nc, board, vis);
+            }
+        }
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+
+        // checking first row
+        for(int i = 0; i < n; i++){
+            if(board[0][i] == 'O'){
+                dfs(0, i, board, vis);
+            }
+        }
+
+        // checking last row
+        for(int i = 0; i < n; i++){
+            if(board[m - 1][i] == 'O'){
+                dfs(m - 1, i, board, vis);
+            }
+        }
+
+        // checking first col
+        for(int i = 0; i < m; i++){
+            if(board[i][0] == 'O'){
+                dfs(i, 0, board, vis);
+            }
+        }
+
+        // checking last col
+        for(int i = 0; i < m; i++){
+            if(board[i][n - 1] == 'O'){
+                dfs(i, n - 1, board, vis);
+            }
+        }
+
+        for(int i = 0; i <m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'O' && !vis[i][j]){
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
